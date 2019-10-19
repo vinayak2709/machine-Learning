9c6b8f4f9ba3ab37a0a0698b8d0ee12b2eded74d
Vinayak Majgaonkar <43469715+vinayak2709@users.noreply.github.com>
2019-10-19 20:20:36 +0530

    Add files via upload

---
+++ trackbar_111.py
@@ -0,0 +1,47 @@
+import cv2
+import imutils
+import numpy as np
+import pytesseract
+
+
+def nothing(x):
+    pass
+
+
+pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+
+cv2.namedWindow("Tracking")
+cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
+
+while True:
+    frame = cv2.imread('ShubamCropped16.jpg')
+    # frame = cv2.resize(frame2,(600,600))
+    cv2.imshow("frame", frame)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    l_h = cv2.getTrackbarPos("LH", "Tracking")
+    l_s = cv2.getTrackbarPos("LS", "Tracking")
+    l_v = cv2.getTrackbarPos("LV", "Tracking")
+
+    u_h = cv2.getTrackbarPos("UH", "Tracking")
+    u_s = cv2.getTrackbarPos("US", "Tracking")
+    u_v = cv2.getTrackbarPos("UV", "Tracking")
+
+    l_b = np.array([l_h, l_s, l_v])
+    u_b = np.array([u_h, u_s, u_v])
+
+    mask = cv2.inRange(hsv, l_b, u_b)
+
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow("mask", mask)
+    result = pytesseract.image_to_string(mask)
+    print(ascii(result))
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
